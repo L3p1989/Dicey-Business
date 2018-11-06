91add3b3265128b98f378e03cec5bdfd80f8fc03
Patrick Hogan <phogan110@live.com>
2018-11-06 09:45:21 -0700

    added `diceRole()` function to randomize a number between 1-6; used randomizer to add `value` to dice

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-let newDie = $("body").append('<button>Generate Die</button>');
+let table = $('body').prepend('<div></div>');
+
+let genDie = $("body").prepend('<button>Generate Die</button>');
+
+function diceRole(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+$('button:first').click(function() {
+    $('div:first').append($('<div class="dice"><p>' + diceRole(1, 7) + '</p></div>'));
+});
