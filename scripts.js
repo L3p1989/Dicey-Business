8de94bb68840f45a9ef199ea5550d4e5b1608052
Patrick Hogan <phogan110@live.com>
2018-11-07 11:38:04 -0700

    fixed `getSum` click to log correct sum of values

--- scripts.js
+++ scripts.js
@@ -1,47 +1,47 @@
 class Die {
     constructor() {
-        this.value;
-        this.div = $('<div class="dice"></div>');
-        this.roll();
-        $(this.div).text(this.value);
-        $('div:first').append(this.div);
+        this.value;//sets value
+        this.div = $('<div class="dice"></div>');//sets created div as this.div
+        this.roll();//sets roll() function to this
+        $(this.div).text(this.value);//sets roll() value to div text
+        $('div:first').append(this.div);//adds new div to dice container
         $(this.div).click(() => {
             this.roll();
             $(this.div).text(this.value);
-        })
+        });//click to run roll() and add the value to this.div's text
     }
     roll() {
         this.value = Math.floor(Math.random() * 6) + 1;
-    }
+    };//generates a value between 1-6
 };
 
-$('body').prepend('<button>Generate Die</button>');
+$('body').prepend('<button>Generate Die</button>');//adds button to beginning of body
 
-let dice = [];
+let dice = [];//creates an array for dice or each die
 
 $('button:first').click(function() {
-    let newDie = new Die();
-    dice.push(newDie);
-    console.log('you is clicking me');
-});
+    let newDie = new Die();//creates a new Die
+    dice.push(newDie);//adds the new die to the dice array
+});//on click do the above
 
-$('button:first').after('<button>Roll Dice</button>');
+$('button:first').after('<button>Roll Dice</button>');//creates another button after 'Generate Die'
 
-$('button:last').after('<div></div>')
+$('button:last').after('<div></div>');//creates the div container for dice
 
 $('button:last').click(function() {
     dice.forEach(die => {
         die.roll();
         die.div.text(die.value);
     });
-});
+});//on click reroll all dice and append a new value to each individual Die
 
-$('div:first').after('<button>Sum</button>');
+$('div:first').after('<button>Sum</button>');//creates a 'Sum' button
 
 $('button:last').click(() => {
-    dice.forEach(die => {
-        let getSum = 0;
-        getSum =+ reduce(die.value);
-        console.log(getSum);
-    })
-})
+    let getSum = 0;
+    dice.forEach(Die => {
+        let value = Die.value
+        getSum += value;
+    });
+    console.log(getSum);
+});//grabs all values and adds them to getSum to log the total value of all the dice on screen
